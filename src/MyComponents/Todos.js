e3b27aecfcd13e6dd3adbcab50a2b51d865ff6b6
Ashhar <ashharali4268@gmail.com>
2022-05-16 18:26:19 +0500

    Update Some Files

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {TodoItem} from "./TodoItem";
 
 export const Todos = (props) => {
   return (
-    <div>
-      {/* {props.todos} */}
+    <div className='conatainer'>
+      <h3 className='text-center'>Todos List</h3>
+      {props.todos.length===0? "No Todos to Display":
+      props.todos.map((todo)=>
+        {
+          return <TodoItem todo={todo} key={todo.Srno} onDelete={props.onDelete} />
+        })
+      } 
+      
+      
+
       </div>
   )
 }
+
+
+
